7ec06177069a140b1dcccf544d7ae91a478d51cc
Christoffer Windahl Madsen <christoffer@codeterraform.com>
2024-11-25 19:59:27 +0100

    Re-releasing

--- Formula/TerrafyARM.rb
+++ Formula/TerrafyARM.rb
@@ -2,7 +2,7 @@ class YourApp < Formula
     desc "Transform Azure ARM templates to Terraform with ease. Export ARM templates via CLI and use TerrafyARM to compile it to valid Terraform code"
     homepage "https://www.codeterraform.com/"
     url "https://github.com/ChristofferWin/TerrafyARM/releases/download/0.1.0-alpha/terrafyarm-macos-amd64.tar.gz"
-    sha256 "your_binary_sha256_hash"
+    sha256 "87da7008de8790b6181b5df079442469641f3505ec5a28cd71326204f12b4e5c"
     license "MIT"
   
     def install
