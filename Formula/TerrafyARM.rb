084b7e14cfd88822fbe285075ffbb21f8326614d
Christoffer Windahl Madsen <christoffer@codeterraform.com>
2024-11-25 01:54:19 +0100

    Binaries for release

--- Formula/TerrafyARM.rb
+++ Formula/TerrafyARM.rb
@@ -1,7 +1,7 @@
 class YourApp < Formula
     desc "Transform Azure ARM templates to Terraform with ease. Export ARM templates via CLI and use TerrafyARM to compile it to valid Terraform code"
     homepage "https://www.codeterraform.com/"
-    url "https://github.com/yourusername/yourproject/releases/download/v1.0.0/your-app.tar.gz"
+    url "https://github.com/ChristofferWin/TerrafyARM/releases/download/0.1.0-alpha/terrafyarm-macos-amd64.tar.gz"
     sha256 "your_binary_sha256_hash"
     license "MIT"
   
